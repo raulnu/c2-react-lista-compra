7f9058674edf5e0d78f1331250a48bfbea163314
Raúl Navarro <raul.navarro.uribe@gmail.com>
2021-06-22 18:03:13 +0200

    Llamada a api y creación de ContextoGeneral

--- src/App.js
+++ src/App.js
@@ -9,29 +9,44 @@ import { PaginaLista } from "./paginas/PaginaLista";
 import { PaginaFormulario } from "./paginas/PaginaFormulario";
 import { PaginaAcercaDe } from "./paginas/PaginaAcercaDe";
 import { Cabecera } from "./components/Cabecera";
+import { useState } from "react";
+import { ContextoGeneral } from "./contexts/ContextoGeneral";
 
 function App() {
+  // llamada API
+
+  const urlAPI = "http://localhost:3001/articulos";
+  const [arrayListado, setArrayListado] = useState([]);
+
+  const llamadaAPI = async () => {
+    const resp = await fetch(urlAPI);
+    const datosResp = await resp.json();
+    setArrayListado(datosResp);
+  };
+
   return (
     <>
       <Router>
-        <Cabecera />
-        <Switch>
-          <Route path="/principal" exact>
-            <PaginaPrincipal />
-          </Route>
-          <Route path="/formulario" exact>
-            <PaginaFormulario />
-          </Route>
-          <Route path="/acerca-de" exact>
-            <PaginaAcercaDe />
-          </Route>
-          <Route path="/lista" exact>
-            <PaginaLista />
-          </Route>
-          <Route path="/" exact>
-            <Redirect to="/principal" />
-          </Route>
-        </Switch>
+        <ContextoGeneral.Provider value={arrayListado}>
+          <Cabecera />
+          <Switch>
+            <Route path="/principal" exact>
+              <PaginaPrincipal />
+            </Route>
+            <Route path="/formulario" exact>
+              <PaginaFormulario />
+            </Route>
+            <Route path="/acerca-de" exact>
+              <PaginaAcercaDe />
+            </Route>
+            <Route path="/lista" exact>
+              <PaginaLista />
+            </Route>
+            <Route path="/" exact>
+              <Redirect to="/principal" />
+            </Route>
+          </Switch>
+        </ContextoGeneral.Provider>
       </Router>
     </>
   );
